62ea84729ecbd182c06f30925fab801cc8240ef2
Wes Okuhara <wokuhara@ucsd.edu>
2015-11-17 10:07:21 -0800

    Refactored Facebook login connection with backend

--- Tunely-Demo/ViewController.swift
+++ Tunely-Demo/ViewController.swift
@@ -45,16 +45,14 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
         print("User Logged In")
         
-        if ((error) != nil)
-        {
+        if ((error) != nil) {
             // Process error
         }
         else if result.isCancelled {
             // Handle cancellations
         }
         else {
-            // If you ask for multiple permissions at once, you
-            // should check if specific permissions missing
+            // If you ask for multiple permissions at once, check if specific permissions missing
             if result.grantedPermissions.contains("email") {
                 returnUserData()
             }
@@ -66,56 +64,68 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         print("User Logged Out")
     }
     
-    func returnUserData()
-    {
+    
+    /** Facebook Login Callback function */
+    func returnUserData() {
         let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"id,email,name,friends, picture.width(480).height(480)"])
         
         graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
             
             if ((error) != nil) {
-                // Process error
                 print("Error: \(error)")
             }
+                
             else {
                 let userName : NSString = result.valueForKey("name") as! NSString
+                let userFbid: String = result.valueForKey("id") as! String
                 let userEmail : NSString = result.valueForKey("email") as! NSString
                 //let userFriends: NSDictionary = result.valueForKey("friends") as! NSDictionary
-                let userFBID: String = result.valueForKey("id") as! String
                 
-                //print(userFriends);
+                // store these variables in the defaults object on the client side
+                defaults.setObject(userName, forKey: "userName")    // userName -> User name
+                defaults.setObject(userFbid, forKey: "userFbid")    // userFbid -> User Facebook ID
+                defaults.setObject(userEmail, forKey: "userEmail")  // userEmail -> User email
                 
-                let uri : String = "http://ec2-54-183-142-37.us-west-1.compute.amazonaws.com/api/users"
+                // Attempt to create a new Facebook user
+                var uri : String = "http://ec2-54-183-142-37.us-west-1.compute.amazonaws.com/api/fbusers"
                 let parameters : [String: AnyObject] = [
                     "name": userName,
                     "email": userEmail,
-                    "fbid": userFBID
+                    "fbid": userFbid
                 ]
                 let headers : [String: String]? = ["x-access-token": FBSDKAccessToken.currentAccessToken().tokenString]
                 
-                Alamofire
-                    .request(.POST, uri, parameters: parameters, headers:headers)
+                Alamofire.request(.POST, uri, parameters: parameters, headers:headers)
                     .responseJSON { json in
                         
-                        let user = JSON(data: json.data!)
+                        var user = JSON(data: json.data!)
                         
-                        // user is already created
-                        if (user["code"]) {
-                            let id = user["op"]["_id"].stringValue
-                            print("User already exists: " + id)
-                            defaults.setObject(id, forKey: "userid")
+                        print(user);
+                        
+                        // Do not proceed if server did not respond
+                        if (user == nil) {
+                            print("No response from server.")
+                            return
+                        }
+                        
+                        // user is already created so update the user
+                        if (user["code"] == 11000) {
+                            print("User: " + (userName as String) + ", already exists.")
                             
-                            let username = user["op"]["name"].stringValue
-                            defaults.setObject(username, forKey: "username")
+                            uri = "http://ec2-54-183-142-37.us-west-1.compute.amazonaws.com/api/fbusers/" + userFbid
+                            
+                            Alamofire.request(.PUT, uri, parameters: parameters, headers:headers)
+                                .responseJSON { json in
+                                    
+                                    user = JSON(data: json.data!)
+                                    
+                                    print(user);
+                            }
                         }
                             
                         // new user created
                         else {
-                            let id = user["_id"].stringValue
-                            print("User created: " + id)
-                            defaults.setObject(id, forKey: "userid")
-                            
-                            let username = user["name"].stringValue
-                            defaults.setObject(username, forKey: "username")
+                            print("New User: " + (userName as String) + ", created.")
                         }
                     }
             }
